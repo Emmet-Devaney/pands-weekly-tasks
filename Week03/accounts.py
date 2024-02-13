cde3126d47d1914b1b94b0b431945117c0749c70
Emmet Devaney <e.devaney6@outlook.com>
2024-02-13 18:40:15 +0000

    Week03 Assessment

---
+++ Week03/accounts.py
@@ -0,0 +1,11 @@
+#When a bank account number of 10 charachters is entered
+#the program will display the last 4 digits
+#while the previous 6 will display X's
+#When a charachter length greater than 10 is entered,
+#if a charachter length greater than 10 is entered the program will only factor the first 10 digits inputted, and display the last 4
+
+account_number = input('please enter your 10 digit account number: ')
+
+displayed_account_number = (account_number[6:10])
+
+print('Your Account Number: XXXXXX' + displayed_account_number)
